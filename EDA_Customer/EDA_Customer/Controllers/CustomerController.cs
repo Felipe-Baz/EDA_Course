e8cdb8c04440a2a488f1996fe40dfcc6f47d7f3a
Felipe Baz <fbazmitsuishi@gmail.com>
2025-02-27 17:44:26 -0300

    customer Controller

---
+++ EDA_Customer/EDA_Customer/Controllers/CustomerController.cs
@@ -0,0 +1,39 @@
+﻿using EDA_Customer.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EDA_Customer.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CustomerController : ControllerBase
+    {
+        public CustomerDBContext _customerDBContext { get; }
+
+        public CustomerController(CustomerDBContext customerDBContext)
+        {
+            _customerDBContext = customerDBContext;
+        }
+
+        [HttpGet]
+        [Route("/customers")]
+        public ActionResult<IEnumerable<Customer>> GetCustomers()
+        {
+            return _customerDBContext.Customers.ToList();
+        }
+
+        [HttpGet]
+        [Route("/products")]
+        public ActionResult<IEnumerable<Product>> GetProducts()
+        {
+            return _customerDBContext.Products.ToList();
+        }
+
+        [HttpPost]
+        public async Task PostCustomer(Customer customer)
+        {
+            _customerDBContext.Customers.Add(customer);
+            await _customerDBContext.SaveChangesAsync();
+        }
+    }
+}
