3ec637799fb9a343dcac3c904191668b8a66bc86
Felipe Baz <fbazmitsuishi@gmail.com>
2025-02-27 17:24:12 -0300

    EDA e-commerce project start

---
+++ EDA_Customer/EDA_Customer/Data/CustomerDBContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EDA_Customer.Data
+{
+    public class CustomerDBContext : DbContext
+    {
+        public CustomerDBContext()
+        {
+            
+        }
+
+        public CustomerDBContext(DbContextOptions dbContextOptions): base(dbContextOptions)
+        {
+           
+        }
+
+        public DbSet<Customer> Customers { get; set; }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
